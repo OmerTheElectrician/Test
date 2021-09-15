c39fda5c7851e418666fcb237b3108cb5222f424
N9_The_Noob_Programmer <48928521+OmerTheElectrician@users.noreply.github.com>
2021-09-15 16:13:37 +0200

    Update and rename main.py to temp_converter.py

--- temp_converter.py
+++ temp_converter.py
@@ -1,19 +1,22 @@
 #super simple temperature converter
 
 def converter():
-    
+
     question = str(input("Do you want to convert Celsius to Fahrenheit or Fahrenheit to Celsius? : ")).upper()
-    
+
     if question == "CELSIUS TO FAHRENHEIT":
         Celsius = float(input("Enter a temperature in Celsius: "))
         Fahrenheit = ((9.0/5.0 * Celsius) + 32)
         print("Temperature in Fahrenheit is: ", Fahrenheit)
-    
-    if question == "FAHRENHEIT TO CELSIUS":
+
+    elif question == "FAHRENHEIT TO CELSIUS":
         Fahrenheit = float(input("Enter a temperature in Fahrenheit: "))
         Celsius = ((Fahrenheit - 32) / 1.8)
         print("Temperature in Celsius is: ", Celsius)
-        
+
+    else:
+        print("Invalid input, aborting.")
+
     quit()
 
 converter()
