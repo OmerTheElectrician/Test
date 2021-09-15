0cf2a7e4b49b4cfab93392f1b5cb52a3426d4319
N9_The_Noob_Programmer <omeraziz@outlook.de>
2021-09-15 14:45:37 +0200

    This looks better

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 #super simple temperature converter
-question = str(input("Do you want to convert Celsius to Fahrenheit or Fahrenheit to Celsius? : "))
-if question == "Celsius to Fahrenheit":
-    Celsius = input("Enter a temperature in Celsius: ")
-    Fahrenheit = ((9.0/5.0 * Celsius) + 32)
-    print("Temperature in Fahrenheit is: ", Fahrenheit)
-if question == "Fahrenheit to Celsius":
-    Fahrenheit = input("Enter a temperature in Fahrenheit: ")
-    Celsius = ((Fahrenheit - 32) / 1.8)
-    print("Temperature in Celsius is: ", Celsius)
+
+def converter():
+    question = str(input("Do you want to convert Celsius to Fahrenheit or Fahrenheit to Celsius? : ")).upper()
+    if question == "CELSIUS TO FAHRENHEIT":
+        Celsius = float(input("Enter a temperature in Celsius: "))
+        Fahrenheit = ((9.0/5.0 * Celsius) + 32)
+        print("Temperature in Fahrenheit is: ", Fahrenheit)
+    if question == "FAHRENHEIT TO CELSIUS":
+        Fahrenheit = float(input("Enter a temperature in Fahrenheit: "))
+        Celsius = ((Fahrenheit - 32) / 1.8)
+        print("Temperature in Celsius is: ", Celsius)
+    quit()    
+        
+converter()
